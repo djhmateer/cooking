46a40421931dabfada2d8d5710dcdf5a3e0dc83e
Dave Mateer <davemateer@gmail.com>
2025-04-09 09:21:49 +0100

    eslint override

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // show warnings in console but wont break production build
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
