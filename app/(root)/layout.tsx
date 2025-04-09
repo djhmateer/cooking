d7323ea9acb886306fedbfab68b8761c7b3255a9
Dave Mateer <davemateer@gmail.com>
2025-04-09 07:49:48 +0100

    Updating code with notes on how Taillwind is working adding comments

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,10 +7,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-screen flex-col">
-      <Header />
-      <main className="flex-1 wrapper">{children}</main>
-      <Footer />
-    </div>
+    <>
+      {/* 
+          h-screen: sets height to 100vh (full viewport height)
+          flex-col: sets flex direction to column (stacks children vertically) */}
+      <div className="flex h-screen flex-col">
+        <Header />
+
+        {/* flex-1: allows this element to grow and take up remaining space
+            which creates a sticky footer
+            wrapper: custom class in globals.css, not a Tailwind class 
+           <main className="flex-1 max-w-7xl lg:mx-auto p-5 md:px-10 w-full"> */}
+        <main className="flex-1 wrapper">{children}</main>
+
+        <Footer />
+      </div>
+    </>
   );
 }
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -10,13 +10,23 @@ const ProductList = ({
   const limitedData = limit ? data.slice(0, limit) : data;
 
   return (
+    // my-10: margin top and bottom of ProductList component of 40px
     <div className="my-10">
+
+      {/* h2-bold: custom heading class */}
+      {/* just: font-bold text-2xl lg:text-3xl; */}
+      {/* mb-4: margin bottom of Title to stuff under it 16px */}
       <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
-        // mobile - 1 column then small 2, medium 3, large 4 columns and up
+        // grid: creates CSS Grid container
+        // grid-cols-1: single column layout by default (mobile)
+        // sm:grid-cols-2: 2 columns at 640px+ screen width
+        // md:grid-cols-3: 3 columns at 768px+ screen width
+        // lg:grid-cols-4: 4 columns at 1024px+ screen width
+        // gap-4: 1rem (16px) spacing between grid items
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {limitedData.map((product: any) => (
-            <div key={product.id}>{product.name}</div>
+            <div key={product.slug}>{product.name}</div>
           ))}
         </div>
       ) : (
