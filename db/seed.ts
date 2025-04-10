7e35b0b07336d60ccc1b02101b553e6cdfd44e49
Dave Mateer <davemateer@gmail.com>
2025-04-10 09:54:35 +0100

    Added dotenv to help in console app

--- db/seed.ts
+++ db/seed.ts
@@ -6,13 +6,16 @@
 
 
 
+// To run this console script
+// npx tsx db/seed
 
 // Lets go direct to the database using no ORM
 // but lets run typescript directly to start with
+import 'dotenv/config';
 import postgres from 'postgres';
 
 console.log("hello world");
-// console.log("process.env.POSTGRES_URL_NON_POOLING", process.env.POSTGRES_URL_NON_POOLING);
+console.log("process.env.POSTGRES_URL_NON_POOLING", process.env.POSTGRES_URL_NON_POOLING);
 
 // const sql = postgres(process.env.POSTGRES_URL_NON_POOLING!, { ssl: 'require' });
 
