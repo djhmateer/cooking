49867581dd909bf66b28bb0906eb6b8bd301c1ea
Dave Mateer <davemateer@gmail.com>
2025-04-11 10:04:02 +0100

    mamking seedpage and seedroute to explore functions on vercel

---
+++ app/seedpage/page.tsx
@@ -0,0 +1,67 @@
+import "dotenv/config";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL_NON_POOLING!);
+// const sql = postgres(process.env.POSTGRES_URL!);
+
+async function noTransactionInsert() {
+  console.time();
+  const customer = {
+    name: "Evil Rabbit",
+    email: "evil@rabbit.com",
+    image_url: "/customers/evil-rabbit.png",
+  };
+
+  try {
+    for (let i = 0; i < 100; i++) {
+      console.log("inserting ", i);
+      await sql`INSERT INTO customers (name, email, image_url)
+        VALUES (${customer.name + i}, ${customer.email}, ${
+        customer.image_url
+      })`;
+    }
+  } catch (error) {
+    console.error("error", error);
+  }
+
+  console.timeEnd();
+}
+
+// 9.5, 8.9, 8.1, 8.9 on non-pooling
+// 8.5, 8.0, 7.4, 8.5, 8.1 on pooling
+async function transactionInsert() {
+  console.time();
+  const customer = {
+    name: "Evil Rabbit",
+    email: "evil@rabbit.com",
+    image_url: "/customers/evil-rabbit.png",
+  };
+
+  try {
+    await sql.begin(async (sql) => {
+      for (let i = 0; i < 100; i++) {
+        console.error("inserting ", i);
+        await sql`
+          INSERT INTO customers (name, email, image_url)
+          VALUES (${customer.name + i}, ${customer.email}, ${
+          customer.image_url
+        })
+        `;
+      }
+    });
+  } catch (error) {
+    console.error("error", error);
+  }
+
+  console.timeEnd();
+}
+
+const FooPage = async () => {
+  const start = Date.now();
+  await noTransactionInsert();
+  const end = Date.now();
+  const duration = end - start;
+  return <>foo in {duration} milliseconds</>;
+};
+
+export default FooPage;
--- app/seedroute/route.ts
+++ app/seedroute/route.ts
@@ -18,7 +18,7 @@ async function noTransactionInsert() {
   try {
     // lets try tons of inserts with no transaction - works fine
     for (let i = 0; i < 100; i++) {
-      console.error("inserting ", i);
+      console.log("inserting ", i);
       await sql`INSERT INTO customers (name, email, image_url)
           VALUES (${customer.name + i}, ${customer.email}, ${
         customer.image_url
@@ -44,7 +44,7 @@ async function transactionInsert() {
   try {
     await sql.begin(async (sql) => {
       for (let i = 0; i < 100; i++) {
-        console.error("inserting ", i);
+        console.log("inserting ", i);
         await sql`
             INSERT INTO customers (name, email, image_url)
             VALUES (${customer.name + i}, ${customer.email}, ${
