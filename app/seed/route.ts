5f035270516e281828f740be5a2cb045bbd15020
Dave Mateer <davemateer@gmail.com>
2025-04-11 09:36:26 +0100

    Added seed.ts which is a console app seeder. Also app/seed/route.ts which is the same but from an API route on next.

---
+++ app/seed/route.ts
@@ -0,0 +1,78 @@
+// import bcrypt from 'bcrypt';
+import postgres from "postgres";
+import { customers } from "../../db/placeholder-data";
+
+const sql = postgres(process.env.POSTGRES_URL_NON_POOLING!);
+// const sql = postgres(process.env.POSTGRES_URL!);
+
+// 10.5, 9.7, 9.2 seconds on non-pooling
+// 11.8, 11.4, 11.4, 8.4 on pooling
+async function noTransactionInsert() {
+  console.time();
+  const customer = {
+    name: "Evil Rabbit",
+    email: "evil@rabbit.com",
+    image_url: "/customers/evil-rabbit.png",
+  };
+
+  try {
+    // lets try tons of inserts with no transaction - works fine
+    for (let i = 0; i < 1000; i++) {
+      console.error("inserting ", i);
+      await sql`INSERT INTO customers (name, email, image_url)
+          VALUES (${customer.name + i}, ${customer.email}, ${
+        customer.image_url
+      })`;
+    }
+  } catch (error) {
+    console.error("error", error);
+  }
+
+  console.timeEnd();
+}
+
+// 9.5, 8.9, 8.1, 8.9 on non-pooling
+// 8.5, 8.0, 7.4, 8.5, 8.1 on pooling
+async function transactionInsert() {
+  console.time();
+  const customer = {
+    name: "Evil Rabbit",
+    email: "evil@rabbit.com",
+    image_url: "/customers/evil-rabbit.png",
+  };
+
+  try {
+    await sql.begin(async (sql) => {
+      for (let i = 0; i < 1000; i++) {
+        console.error("inserting ", i);
+        await sql`
+            INSERT INTO customers (name, email, image_url)
+            VALUES (${customer.name + i}, ${customer.email}, ${
+          customer.image_url
+        })
+          `;
+      }
+    });
+  } catch (error) {
+    console.error("error", error);
+  }
+
+  console.timeEnd();
+}
+
+export async function GET() {
+  try {
+    const start = Date.now();
+    await noTransactionInsert();
+    // transactionInsert();
+    const end = Date.now();
+    const duration = end - start;
+
+    return Response.json({
+      message: `seeded successfully from route in ${duration} milliseconds`,
+    });
+  } catch (error) {
+    console.log("error", error);
+    return Response.json({ error }, { status: 500 });
+  }
+}
--- db/seed.ts
+++ db/seed.ts
@@ -2,13 +2,24 @@
 // To run this console script
 // npx tsx db/seed
 
-// Lets go direct to the database using no ORM
 import "dotenv/config";
 import postgres from "postgres";
 // from nextjs-dashboard project
 import { customers } from "./placeholder-data";
 
-const sql = postgres(process.env.POSTGRES_URL_NON_POOLING!, { ssl: "require" });
+const sql = postgres(process.env.POSTGRES_URL_NON_POOLING!);
+// const sql = postgres(process.env.POSTGRES_URL!);
+
+// can get the sql outputted, including parameters,but not the query time
+// const sql = postgres(process.env.POSTGRES_URL_NON_POOLING!, {
+//   ssl: 'require',
+//   debug: (connection: number, query: string, parameters: any[], paramTypes: any[]) => {
+//     console.log(' PG Executed query:', query);
+//     if (parameters && parameters.length > 0) {
+//       console.log(' PG parameters:', parameters);
+//     }
+//   },
+// });
 
 async function seedCustomers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -35,7 +46,6 @@ async function seedCustomers() {
 }
 
 async function main() {
-  // Drop all existing tables
   console.log("dropping and creating public schema");
   await sql`DROP SCHEMA public CASCADE;`;
   await sql`CREATE SCHEMA public;`;
@@ -57,11 +67,63 @@ async function main() {
   console.log("end transaction");
 }
 
-main();
+// 10.5, 9.7, 9.2 seconds on non-pooling
+// 11.8, 11.4, 11.4, 8.4 on pooling
+async function noTransactionInsert() {
+  console.time();
+  const customer = {
+    name: "Evil Rabbit",
+    email: "evil@rabbit.com",
+    image_url: "/customers/evil-rabbit.png",
+  };
+
+  try {
+    // lets try tons of inserts with no transaction - works fine
+    for (let i = 0; i < 1000; i++) {
+      console.error("inserting ", i);
+      await sql`INSERT INTO customers (name, email, image_url)
+        VALUES (${customer.name+i}, ${customer.email}, ${customer.image_url})`;
+    }
+  } catch (error) {
+    console.error("error", error);
+  }
+
+  console.timeEnd();
+}
+
+// 9.5, 8.9, 8.1, 8.9 on non-pooling
+// 8.5, 8.0, 7.4, 8.5, 8.1 on pooling
+async function transactionInsert() {
+  console.time();
+  const customer = {
+    name: "Evil Rabbit",
+    email: "evil@rabbit.com",
+    image_url: "/customers/evil-rabbit.png",
+  };
+
+  try {
+    await sql.begin(async (sql) => {
+      for (let i = 0; i < 1000; i++) {
+        console.error("inserting ", i);
+        await sql`
+          INSERT INTO customers (name, email, image_url)
+          VALUES (${customer.name + i}, ${customer.email}, ${customer.image_url})
+        `;
+      }
+    });
+  } catch (error) {
+    console.error("error", error);
+  }
+
+  console.timeEnd();
+}
+
+// noTransactionInsert();
+transactionInsert();
+
 
 // Next is to use pooler
 // then try drizzle
-
 
 //// import { drizzle } from 'drizzle-orm/postgres-js'
 
