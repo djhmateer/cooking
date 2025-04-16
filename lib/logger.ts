56adec94f49f4887ae01c401ea4cfcc20c4a1f5a
Dave Mateer <davemateer@gmail.com>
2025-04-16 07:48:53 +0100

    force trace

--- lib/logger.ts
+++ lib/logger.ts
@@ -2,26 +2,27 @@
 import pino, { LoggerOptions } from 'pino';
 
 // todo try these options: https://stackoverflow.com/a/78435769/26086
-// const options: LoggerOptions = {
-//   // level: process.env.LOG_LEVEL || 'info',
-//   level: 'trace',
-//   transport:
-//     process.env.NODE_ENV === 'development'
-//       ? {
-//           target: 'pino-pretty',
-//           options: {
-//             colorize: true,
-//             translateTime: 'SYS:standard',
-//             ignore: 'pid,hostname',
-//           },
-//         }
-//       : undefined,
-// };
+const options: LoggerOptions = {
+  // level: process.env.LOG_LEVEL || 'info',
+  level: 'trace',
+  // transport:
+  //   process.env.NODE_ENV === 'development'
+  //     ? {
+  //         target: 'pino-pretty',
+  //         options: {
+  //           colorize: true,
+  //           translateTime: 'SYS:standard',
+  //           ignore: 'pid,hostname',
+  //         },
+  //       }
+  //     : undefined,
+};
 
-// const log = pino(options);
+const log = pino(options);
 
 // best performance for logging to stdout
 // https://getpino.io/#/docs/help?id=best-performance-for-logging-to-stdout
-const log = pino();
+// default is LOG_LEVEL of 30 ie Info
+// const log = pino();
 
 export default log;
