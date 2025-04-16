40ceb282938729a42dc459276a72acafef4c491f
Dave Mateer <davemateer@gmail.com>
2025-04-16 07:21:51 +0100

    min log level on prod change to debug

--- lib/logger.ts
+++ lib/logger.ts
@@ -3,7 +3,8 @@ import pino, { LoggerOptions } from 'pino';
 
 // todo try these options: https://stackoverflow.com/a/78435769/26086
 const options: LoggerOptions = {
-  level: process.env.LOG_LEVEL || 'info',
+  // level: process.env.LOG_LEVEL || 'info',
+  level: process.env.LOG_LEVEL || 'debug',
   transport:
     process.env.NODE_ENV === 'development'
       ? {
