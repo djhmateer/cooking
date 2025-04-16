f03ada4b5b1d42ed66207d0eee2334dac522cc75
Dave Mateer <davemateer@gmail.com>
2025-04-16 07:53:25 +0100

    patch back in pretty for dev

--- lib/logger.ts
+++ lib/logger.ts
@@ -5,17 +5,18 @@ import pino, { LoggerOptions } from 'pino';
 const options: LoggerOptions = {
   // level: process.env.LOG_LEVEL || 'info',
   level: 'trace',
-  // transport:
-  //   process.env.NODE_ENV === 'development'
-  //     ? {
-  //         target: 'pino-pretty',
-  //         options: {
-  //           colorize: true,
-  //           translateTime: 'SYS:standard',
-  //           ignore: 'pid,hostname',
-  //         },
-  //       }
-  //     : undefined,
+  // if development, use pino-pretty to colorize the logs
+  transport:
+    process.env.NODE_ENV === 'development'
+      ? {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname',
+          },
+        }
+      : undefined,
 };
 
 const log = pino(options);
