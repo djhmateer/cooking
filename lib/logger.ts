2af702d13a38d98b46e707deeae44fa11b37fac0
Dave Mateer <davemateer@gmail.com>
2025-04-20 08:20:11 +0100

    splitting apart production and development for pino-pretty

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,15 +1,14 @@
 // lib/logger.ts
-import pino, { LoggerOptions } from 'pino';
-import type { LokiOptions } from 'pino-loki'
+import pino, { LoggerOptions } from "pino";
+import type { LokiOptions } from "pino-loki";
 
 // stops runtime errors on vercel
-import '@logtail/pino'
-
+import "@logtail/pino";
 
 // todo try these options: https://stackoverflow.com/a/78435769/26086
 const options: LoggerOptions = {
   // unless overridden by LOG_LEVEL env var, use trace level
-  level: process.env.LOG_LEVEL || 'trace',
+  level: process.env.LOG_LEVEL || "trace",
   // level: 'trace',
   // if development, use pino-pretty to colorize the logs
   // transport:
@@ -25,11 +24,10 @@ const options: LoggerOptions = {
   //     : undefined,
 };
 
-
 // loki and grafana cloud test - can't get it to work
 // const transport = pino.transport<LokiOptions>({
 //     // target: 'pino-pretty', // for simple stdout debugging
-    
+
 //   target: "pino-loki",
 //   options: {
 //     batching: true,
@@ -40,7 +38,7 @@ const options: LoggerOptions = {
 //     host: 'https://logs-prod-035.grafana.net',
 //     basicAuth: {
 //       // username: process.env.LOKI_USERNAME || "username",
-//       username: "number", 
+//       username: "number",
 //       password: process.env.LOKI_PASSWORD || "password",
 //       // password: "asdf",
 //     },
@@ -65,26 +63,37 @@ const sourceToken = process.env.LOGTAIL_SOURCE_TOKEN;
 //   },
 // });
 
+let transport;
+
 // Prety and Logtail - multi transport test works
-const transport = pino.transport({
-  targets: [
-    {
-      target: "pino-pretty",
-      options: {
-        colorize: true,
-        translateTime: 'SYS:standard',
-        ignore: 'pid,hostname',
-      }
-    },
-    {
-      target: "@logtail/pino",
-      options: {
-        sourceToken: sourceToken,
-        options: { endpoint: ingestingHost },
-      }
-    },
-  ],
-});
+if (process.env.NODE_ENV === "development") {
+  transport = pino.transport({
+    targets: [
+      {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "SYS:standard",
+          ignore: "pid,hostname",
+        },
+      },
+      {
+        target: "@logtail/pino",
+        options: {
+          sourceToken: sourceToken,
+          options: { endpoint: ingestingHost },
+        },
+      },
+    ],
+  });
+}
+
+if (process.env.NODE_ENV === "production") {
+  transport = pino.transport({
+    target: "@logtail/pino",
+    options: { sourceToken: sourceToken, options: { endpoint: ingestingHost } },
+  });
+}
 
 // const baseOptions: LoggerOptions = {
 //   level: process.env.LOG_LEVEL || 'trace',
