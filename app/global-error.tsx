f892b75ce4e8bef3cefb22919fe3fe18a16ce5b8
Dave Mateer <davemateer@gmail.com>
2025-04-22 13:37:38 +0100

    adding sentry

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,33 +1,51 @@
-'use client' // Error boundaries must be Client Components
- 
-import { useEffect } from 'react'
+"use client"; // Error boundaries must be Client Components
+
 import Link from "next/link";
- 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string }
-  reset: () => void
-}) {
-  console.log("Global error - logging to stdout");
+// import { useEffect } from 'react'
+
+// export default function Error({
+//   error,
+//   reset,
+// }: {
+//   error: Error & { digest?: string }
+//   reset: () => void
+// }) {
+//   console.log("Global error - logging to stdout");
+//   useEffect(() => {
+//     // Log the error to an error reporting service
+//     console.error(error);
+//   }, [error]);
+
+//   return (
+//     <div>
+//       <h2>Something went wrong!</h2>
+//       <Link href="/">Go Home</Link>
+//       {/* <button
+//         onClick={
+//           // Attempt to recover by trying to re-render the segment
+//           () => reset()
+//         }
+//       >
+//         Try again
+//       </button> */}
+//     </div>
+//   )
+// }
+
+// Sentry
+import * as Sentry from "@sentry/nextjs";
+import Error from "next/error";
+import { useEffect } from "react";
+
+export default function GlobalError({ error }: { error: Error }) {
   useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
+    Sentry.captureException(error);
   }, [error]);
- 
+
   return (
     <div>
       <h2>Something went wrong!</h2>
       <Link href="/">Go Home</Link>
-      {/* <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button> */}
     </div>
-  )
+  );
 }
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://683b51f5b2c534b7aa5891d8cfec2a71@o4508385971273728.ingest.de.sentry.io/4509196917735504",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
