be18516ef45f2d8f2d435ecd4191fdda9084b872
Dave Mateer <davemateer@gmail.com>
2025-04-23 11:33:00 +0100

    commented out config to see double call of sampleerror page

--- app/sample-error/page.tsx
+++ app/sample-error/page.tsx
@@ -1,40 +1,11 @@
 // dont want this to be run on deployment and cached
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
-import "dotenv/config";
-// import postgres from "postgres";
-
-// const sql = postgres(process.env.POSTGRES_URL_NON_POOLING!);
-
-// async function transactionInsert() {
-//   console.time();
-//   const customer = {
-//     name: "Evil Rabbit",
-//     email: "evil@rabbit.com",
-//     image_url: "/customers/evil-rabbit.png",
-//   };
-
-//   try {
-//     await sql.begin(async (sql) => {
-//       for (let i = 0; i < 1000; i++) {
-//         // console.log("inserting ", i);
-//         await sql`
-//           INSERT INTO customers (name, email, image_url)
-//           VALUES (${customer.name + i}, ${customer.email}, ${
-//           customer.image_url
-//         })
-//         `;
-//       }
-//     });
-//   } catch (error) {
-//     console.log("error", error);
-//   }
-
-//   console.timeEnd();
-// }
+// import "dotenv/config";
 
 const SampleErrorPage = async () => {
-  throw new Error("Sample Error");
+  console.warn("SampleErrorPage being called but am not throwing an error up to global error handler");
+  // throw new Error("Sample Error");
 
   return <>Sample Error Page text</>;
 };
