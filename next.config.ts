12bde1e71bbcc42e0ec8ba6f408778c88e9fdbd5
Dave Mateer <davemateer@gmail.com>
2025-04-23 13:14:58 +0100

    update package json to remove _notUsed

--- next.config.ts
+++ next.config.ts
@@ -1,46 +1,4 @@
-// import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
-
-// Axiom
-// const { withAxiom } = require('next-axiom');
-
-// const nextConfig = withAxiom({
-//   // Your existing configuration.
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-// });
-
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-
-//   // show warnings in console but wont break production build
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-
-//   // Next.js 15
-//   // serverExternalPackages: ["pino", "pino-pretty", "@logtail/pino", "thread-stream"],
-
-//   // // resolves client-side module resolution error for pino
-//   // // @see https://github.com/pinojs/pino/issues/1841#issuecomment-2244564289
-//   // webpack: (config, context) => {
-//   //   config.externals.push({
-//   //     "thread-stream": "commonjs thread-stream",
-//   //   });
-//   //   return config;
-//   // },
-// };
-
-// default next config
-// const nextConfig = {
-//   // Your existing configuration.
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-// };
-// export default nextConfig;
 
 const nextConfig = {
   eslint: {
