22c4c4a576fbf1fc2c04a34716639a35f83e5209
Dave Mateer <davemateer@gmail.com>
2025-04-24 11:58:06 +0100

    gone back to webpack for prod and taken out async for homepage (will put back in later)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
 import sampleData from "@/db/sample-data";
 import ProductList from "@/components/product-list";
 
-const Homepage = async () => {
+// const Homepage = async () => {
+const Homepage = () => {
   return (
     <>
       <ProductList data={sampleData.products} title="Top Recipes" limit={4} />
