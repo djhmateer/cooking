2c06a674251bb804cf32e7a7deb288a762bf14eb
Dave Mateer <davemateer@gmail.com>
2025-04-25 16:44:22 +0100

    errors writing to console in sampleerrorpage

--- app/(root)/sample-error/page.tsx
+++ app/(root)/sample-error/page.tsx
@@ -3,9 +3,10 @@ export const dynamic = "force-dynamic";
 // not async unless need it
 export default function SampleErrorPage() { 
   try { 
-    throw new Error("This is a sample error");
+    // this gets logged to the console on vercel!?
+    throw new Error("sample error in theserver component");
   } catch (error) {
-    console.error("sample-error page in try catch - logging to stderr and rethrowing", error);
+    console.error("in try catch.. with caught error included ", error);
     throw error;
   }
   return <>Sample Error Page text</>;
