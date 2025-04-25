bfd6aff48daa8074ed0abedc31693dce074b4c53
Dave Mateer <davemateer@gmail.com>
2025-04-25 16:54:58 +0100

    took out superfluous error messages

--- app/(root)/sample-error/page.tsx
+++ app/(root)/sample-error/page.tsx
@@ -3,10 +3,12 @@ export const dynamic = "force-dynamic";
 // not async unless need it
 export default function SampleErrorPage() { 
   try { 
-    // this gets logged to the console on vercel!?
+    // this gets logged to the console on vercel
     throw new Error("sample error in theserver component");
   } catch (error) {
-    console.error("in try catch.. with caught error included ", error);
+    // this gets logged first in the sample function error
+    // console.error("in try catch.. with caught error included ", error);
+    console.error("in try catch.. don't need to output the error again");
     throw error;
   }
   return <>Sample Error Page text</>;
