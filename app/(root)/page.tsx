0216bfdb5107e6f4eb1ad6620c19061bc90514ae
Dave Mateer <davemateer@gmail.com>
2025-04-28 09:01:33 +0100

    console output test

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,7 +6,10 @@ import ProductList from "@/components/product-list";
 // const Homepage = async () => {
 const Homepage = () => {
   // server component - so output to webserver output and not the client console (use client will do this)
-  console.log("data is ", sampleData);
+  console.log("log data is ", sampleData);
+  console.info("info data is ", sampleData);
+  console.warn("warn data is ", sampleData);
+  console.error("error data is ", sampleData);
   return (
     <>
       Currently we're importing sample data from a /db/sample-data.ts file.
